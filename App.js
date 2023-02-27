51fe2a8ad05a7e6c713aa82facab883dbeebefb3
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-02-27 22:52:42 +0530

    Miscellaneous Changes

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+  ? To run:
+  1) cd DoneWithIt
+  2) npx expo start
+  3) Press 'a' to run on Android emulator
+  4) Press 'w' to run on web browser
+
+  ? Open android virtual device (AVD):
+  1) Open Android Studio
+  2) Open AVD Manager
+  3) Start AVD
+  
+  ? To open developer menu on an Android emulator:
+  1) Press ctrl + m on emulator. On mobile, shake device.
+
+  ? To publish:
+  1) expo publish
+
+  ? Project Link:
+  https://expo.dev/@hardvan/DoneWithIt?serviceType=classic&distribution=expo-go
+*/
+
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log("Console log message, this is.");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, World! This is React Native.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
