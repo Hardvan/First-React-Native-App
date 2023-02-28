e6c94db25c7a07005faa3995795f6e4a499f65ce
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-02-28 12:37:57 +0530

    Add ViewImageScreen

--- App.js
+++ App.js
@@ -24,7 +24,8 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
